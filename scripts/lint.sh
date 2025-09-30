f1f79f9ecaf461a245d72f6240eedc819f1a4b2d
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-09-29 23:23:05 -0300

    dev: adiciona .local ao ignore do linter

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,7 +4,7 @@
 set -ev
 
 cd /app
-autoflake --in-place --recursive --remove-unused-variables --remove-all-unused-imports --exclude docker/,.git/ .
+autoflake --in-place --recursive --remove-unused-variables --remove-all-unused-imports --exclude docker/,.git/,.local .
 isort --line-length 120 --multi-line VERTICAL_HANGING_INDENT --trailing-comma .
-black --exclude '(docker/|\.direnv|\.eggs|\.git|\.mypy_cache|\.nox|\.tox|\.venv|venv|\.ipynb_checkpoints|_build|buck-out|build|dist|__pypackages__)' -l 120 .
+black --exclude '(docker/|\.direnv|\.eggs|\.git|\.local|\.mypy_cache|\.nox|\.tox|\.venv|venv|\.ipynb_checkpoints|_build|buck-out|build|dist|__pypackages__)' -l 120 .
 flake8 --config setup.cfg
