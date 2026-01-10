fd7930b83d72e4401a76ee7617c275635a17fe4b
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2026-01-10 13:54:54 -0300

    ibge: corrige smoke test para cobrir IPCA

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -188,7 +188,7 @@ python -m mercados.b3 clearing-termo-eletronico "$DATA_INICIAL" "$DATA_PATH/b3-c
 
 # IBGE
 echo mercados.ibge historico
-python -m mercados.ibge historico "IPCA-15" -i "2025-01-01" -F "csv" > "$DATA_PATH/ibge-historico-ipca-2025.csv"
+python -m mercados.ibge historico "IPCA" -i "2025-01-01" -F "csv" > "$DATA_PATH/ibge-historico-ipca-2025.csv"
 python -m mercados.ibge historico -i "2025-01-01" "IPCA-15" "$DATA_PATH/ibge-historico-ipca15-2025.csv"
 
 
